cabd08c698d1e4d9d47a8ff0b848e83c72c8107d
Steven Perez <stevenalp18@gmail.com>
2023-09-25 21:51:11 -0400

    simple node server

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
-// Import http module
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
+require("dotenv").config();
 
 // Declare port for server
-const port = 3000;
+// process.env.PORT = 3000;
+// const port = process.env.PORT;
+
+// Decalred with environmental variables to save certain data
+const port = process.env.PORT;
 
 // Create Server
 const server = http.createServer((req, res) => {
   // Write the response
-  res.write("This is the response from the server");
-  res.end();
+  if (req.url === "/" || req.url === "index.html") {
+    const filePath = path.join(__dirname, "/index.html");
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 Not Found");
+  }
 });
 
 server.listen(port, () => {
